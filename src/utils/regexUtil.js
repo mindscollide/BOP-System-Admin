65b53c9ac0280fd918529bcb22cca79518755f8b
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2025-06-24 11:26:13 +0500

    Update regexUtil.js

--- src/utils/regexUtil.js
+++ src/utils/regexUtil.js
@@ -4,6 +4,6 @@ export function validateEmail(email) {
 }
 
 export function validateBopEmail(email) {
-  const pattern = /^[^\s@]+@bop\.com(\.pk)?$/;
+  const pattern = /^[^\s@]+@bop\.com(\.pk)?$/i;
   return pattern.test(email);
 }
