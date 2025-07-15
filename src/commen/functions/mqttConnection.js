b10c1e86cc61b3ae55be17e2237e9690dc5512fb
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2025-07-15 12:03:22 +0500

    code push

--- src/commen/functions/mqttConnection.js
+++ src/commen/functions/mqttConnection.js
@@ -113,7 +113,7 @@ export const useMqttClient = ({
         userName: process.env.REACT_APP_MQTT_USERNAME,
         password: process.env.REACT_APP_MQTT_PASSWORD,
         cleanSession: true,
-        useSSL: false,
+        useSSL: process.env.REACT_APP_MQTT_PORT === "8883" ? true : false,
       });
     },
     [onMessageArrived, onConnectionLost, randomString, subscribeToTopics]
