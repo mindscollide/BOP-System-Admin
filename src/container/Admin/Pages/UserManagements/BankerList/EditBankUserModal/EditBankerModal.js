e7cc9fcca9c4749cd035160433054f9b55129269
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2025-07-18 19:55:15 +0500

    Update EditBankerModal.js

--- src/container/Admin/Pages/UserManagements/BankerList/EditBankUserModal/EditBankerModal.js
+++ src/container/Admin/Pages/UserManagements/BankerList/EditBankUserModal/EditBankerModal.js
@@ -482,10 +482,12 @@ const EditBankerModal = () => {
                       <span className={styles["aesterick-color"]}>*</span>
                     </span>
                     <Select
+                    
                       options={branchOptions}
                       placeholder="Select Branch"
                       value={branchRole.value !== 0 ? branchRole : null}
                       onChange={branchSelectRoleHandler}
+                      isDisabled={true}
                       isSearchable={true}
                       classNamePrefix="selectCateogyCorporateList"
                       menuPortalTarget={document.body}
