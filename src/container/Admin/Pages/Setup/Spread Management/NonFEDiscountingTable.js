5f04cabbb35e59e44c6231e6ef0033536fd31b8e
aunrazanaqvi <106374006+aunrazanaqvi@users.noreply.github.com>
2025-07-21 19:31:46 +0500

    code push

--- src/container/Admin/Pages/Setup/Spread Management/NonFEDiscountingTable.js
+++ src/container/Admin/Pages/Setup/Spread Management/NonFEDiscountingTable.js
@@ -21,6 +21,12 @@ const NonFEDiscountingTable = ({ categoryID }) => {
   const [modalState, setModalState] = useState(0);
   const [NonFEDiscoutingData, setNonFEDiscoutingData] = useState([]);
   const [FEDiscoutingColumns, setFEDiscoutingColumns] = useState([]);
+  console.log(FEDiscoutingColumns, "FEDiscoutingColumns");
+  console.log(
+    GetTenorWiseNonFEDiscountingSpreadsForCategory,
+    "GetTenorWiseNonFEDiscountingSpreadsForCategory"
+  );
+
   const GetTenorWiseNonFEDiscountingSpreadsForCategory = useSelector(
     (state) =>
       state.SpreadManagementReducer
@@ -52,6 +58,11 @@ const NonFEDiscountingTable = ({ categoryID }) => {
             handleChangeDiscounting
           );
           setFEDiscoutingColumns(columnsData);
+          console.log(columnsData, "columnsDatacolumnsData");
+          console.log(
+            GetTenorWiseNonFEDiscountingSpreadsForCategory,
+            "GetTenorWiseNonFEDiscountingSpreadsForCategory"
+          );
 
           setNonFEDiscoutingData(rowData);
         } catch (error) {}
