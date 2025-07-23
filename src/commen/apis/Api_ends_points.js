732c83eec123f032348a8364548248934771d658
SaifULIslamMindsCollide <124796479+SaifULIslamMindsCollide@users.noreply.github.com>
2025-07-23 18:41:25 +0500

    cc

--- src/commen/apis/Api_ends_points.js
+++ src/commen/apis/Api_ends_points.js
@@ -6,7 +6,6 @@ const authenticationAPI = `${baseURL}${process.env.REACT_APP_AUTH_PORT}`;
 const systemAdminAPI = `${baseURL}${process.env.REACT_APP_SYSTEM_ADMIN_PORT}`;
 const securityAdminAPI = `${baseURL}${process.env.REACT_APP_SECURITY_ADMIN_PORT}`;
 const downloadReportAPI = `${baseURL}${process.env.REACT_APP_DOWNLOAD_REPORT_PORT}`;
-//const downloadReportAPI = "https://localhost:44323/ExcelReport";
 const settingsAPI = `${baseURL}${process.env.REACT_APP_SETTINGS_PORT}`;
 const uploadRateAPI = `${baseURL}${process.env.REACT_APP_UPLOAD_RATE_PORT}`;
 
