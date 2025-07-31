0c7d20a8b5beb350322a4041db561c9716f1f9ec
Ali Raza Mamdani <Ali.mamdani@hotmail.com>
2025-07-31 17:39:26 +0500

    Update CurrencyModal.js

--- src/container/Admin/Pages/Setup/CurrencyManagement/CurrencyManagementModal/CurrencyModal.js
+++ src/container/Admin/Pages/Setup/CurrencyManagement/CurrencyManagementModal/CurrencyModal.js
@@ -125,6 +125,7 @@ const CurrencyModal = () => {
             <Col lg={12} md={12} sm={12} className={styles["EditBranchLabel"]}>
               <Checkbox
                 checked={formState.NonFEDiscountingApplicable}
+                disabled={true}
                 onChange={() =>
                   handleCheckboxChange("NonFEDiscountingApplicable")
                 }
